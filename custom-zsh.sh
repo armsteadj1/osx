b64f79d3a241d6056c387f464d018610934b604d
James Armstead <james.armstead@hey.com>
2020-12-07 13:45:15 -0600

    moving all zsh stuff and adding a last step echo

--- custom-zsh.sh
+++ custom-zsh.sh
@@ -1,9 +1,16 @@
-git clone git@github.com:starcraftman/zsh-git-prompt.git custom/plugins/zsh-git-prompt
-git clone https://github.com/lukechilds/zsh-nvm custom/plugins/zsh-nvm
-git clone https://github.com/zsh-users/zsh-autosuggestions custom/plugins/zsh-autosuggestions
+git clone git@github.com:starcraftman/zsh-git-prompt.git zsh/custom/plugins/zsh-git-prompt
+git clone https://github.com/lukechilds/zsh-nvm zsh/custom/plugins/zsh-nvm
+git clone https://github.com/zsh-users/zsh-autosuggestions zsh/custom/plugins/zsh-autosuggestions
+
+rm -r custom.old
+rm .zshrc.old
 
 mv $ZSH/custom custom.old
 mv ~/.zshrc .zshrc.old
 
-ln .zshrc ~/.zshrc
-ln -s ~/osx/custom $ZSH/custom
+ln ~/osx/zsh/.zshrc ~/.zshrc
+ln -s ~/osx/zsh/custom $ZSH/custom
+
+echo "****"
+echo "****run 'source ~/.zshrc' to finish setup****"
+echo "****"
